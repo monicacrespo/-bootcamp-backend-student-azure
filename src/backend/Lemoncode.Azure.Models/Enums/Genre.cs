d0c7d1b996efd8bfb6ba5b513c4f9fc62f97aa8e
monicacrespo <moni.crespo@gmail.com>
2023-05-01 18:36:04 +0100

    Initial version

---
+++ src/backend/Lemoncode.Azure.Models/Enums/Genre.cs
@@ -0,0 +1,15 @@
+namespace Lemoncode.Azure.Models.Enums
+{
+    public enum Genre
+    {
+        Action,
+        ActionAdventure,
+        Adventure,
+        Puzzle,
+        RolePlaying,
+        Simulation,
+        Strategy,
+        Sports,
+        MMO
+    }
+}
